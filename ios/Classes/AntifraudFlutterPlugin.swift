08c2110340faf45444453001babb27597c72420a
shahzodatabayev <shatabaev@beeline.uz>
2024-12-11 12:52:12 +0500

    feat: Ios module

--- ios/Classes/AntifraudFlutterPlugin.swift
+++ ios/Classes/AntifraudFlutterPlugin.swift
@@ -1,19 +1,127 @@
 import Flutter
 import UIKit
+import AntiFraud_Mobile_SDK
 
 public class AntifraudFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "antifraud_flutter", binaryMessenger: registrar.messenger())
-    let instance = AntifraudFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    private var library: AntiFraudLibrary?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "antifraud_flutter", binaryMessenger: registrar.messenger())
+        let instance = AntifraudFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "initialize":
+            guard let args = call.arguments as? [String: Any],
+                  let host = args["host"] as? String,
+                  let tokenType = args["token_type"] as? String,
+                  let accessToken = args["access_token"] as? String else {
+                result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments passed", details: nil))
+                return
+            }
+            initialize(host: host, tokenType: tokenType, accessToken: accessToken, result: result)
+            
+        case "verify_sms_code":
+            guard let args = call.arguments as? [String: Any],
+                  let phoneNumber = args["phone_number"] as? String,
+                  let smsCode = args["code"] as? String else {
+                result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments passed", details: nil))
+                return
+            }
+            verifySmsCode(phoneNumber: phoneNumber, smsCode: smsCode, result: result)
+            
+        case "logout":
+            logout(result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func initialize(host: String, tokenType: String, accessToken: String, result: @escaping FlutterResult) {
+        library = AntiFraudLibrary(host: host)
+        
+        library?.initialization { response in
+            switch response {
+            case .success:
+                result("SDK Initialized successfully")
+            case .failure(let error):
+                result(FlutterError(code: "INIT_ERROR", message: error.localizedDescription, details: nil))
+            @unknown default:
+                result(FlutterError(code: "INIT_ERROR", message: "BASE_ERROR", details: nil))
+            }
+        }
+    }
+    
+    private func verifySmsCode(phoneNumber: String, smsCode: String, result: @escaping FlutterResult) {
+        guard let library = library else {
+            result(FlutterError(code: "INIT_ERROR", message: "SDK is not initialized", details: nil))
+            return
+        }
+        library.verifySmsCode(phoneNumber: phoneNumber, smsCode: smsCode) { response in
+            switch response {
+            case .success:
+                result("Verified successfully")
+            case .failure(let error):
+                result(FlutterError(code: "VERIFY_ERROR", message: error.localizedDescription, details: nil))
+            @unknown default:
+                result(FlutterError(code: "INIT_ERROR", message: "BASE_ERROR", details: nil))
+            }
+
+        }
+    }
+    
+    private func confirmFace(birthDate: String, document: String, result: @escaping FlutterResult) {
+        guard let library = library else {
+            result(FlutterError(code: "INIT_ERROR", message: "SDK is not initialized", details: nil))
+            return
+        }
+        library.confirmFace(birthDate: birthDate, document: document) { response in
+            switch response {
+            case .success:
+                result("Confirmed successfully")
+            case .failure(let error):
+                result(FlutterError(code: "CONFIRM_FACE_ERROR", message: error.localizedDescription, details: nil))
+            @unknown default:
+                result(FlutterError(code: "INIT_ERROR", message: "BASE_ERROR", details: nil))
+            }
+    
+        }
+    }
+    
+    private func detectFraud(smsCode: String, result: @escaping FlutterResult) {
+        guard let library = library else {
+            result(FlutterError(code: "INIT_ERROR", message: "SDK is not initialized", details: nil))
+            return
+        }
+        library.detectFraud(smsCode: smsCode) { response in
+            switch response {
+            case .success:
+                result("Transaction verified successfully")
+            case .failure(let error):
+                result(FlutterError(code: "DETECT_FRAUD_ERROR", message: error.localizedDescription, details: nil))
+            @unknown default:
+                result(FlutterError(code: "INIT_ERROR", message: "BASE_ERROR", details: nil))
+            }
+
+        }
+    }
+    
+    private func logout(result: @escaping FlutterResult) {
+        guard let library = library else {
+            result(FlutterError(code: "INIT_ERROR", message: "SDK is not initialized", details: nil))
+            return
+        }
+        library.logout { response in
+            switch response {
+            case .success:
+                result("Logged out successfully")
+            case .failure(let error):
+                result(FlutterError(code: "LOGOUT_ERROR", message: error.localizedDescription, details: nil))
+            @unknown default:
+                result(FlutterError(code: "INIT_ERROR", message: "BASE_ERROR", details: nil))
+            }
+        }
+    }
 }
