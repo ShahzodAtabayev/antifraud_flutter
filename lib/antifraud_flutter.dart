ba01146ac91930f95155959687647b1894508ee5
shatabaev <shatabaev@beeline.uz>
2025-10-07 14:54:56 +0500

    change: Android sdk to 1.1.0, ios sdk 1.0.7

--- lib/antifraud_flutter.dart
+++ lib/antifraud_flutter.dart
@@ -3,33 +3,31 @@ import 'package:either_dart/either.dart';
 import 'antifraud_flutter_platform_interface.dart';
 
 sealed class AntifraudFlutter {
-  const AntifraudFlutter._();
-
-  static Future<Either<String, void>> initialize({required String host}) {
+  Future<Either<String, void>> initialize({required String host}) {
     return AntifraudFlutterPlatform.instance.initialize(host: host);
   }
 
-  static Future<Either<String, bool>> isInitialized() {
+  Future<Either<String, bool>> isInitialized() {
     return AntifraudFlutterPlatform.instance.isInitialized();
   }
 
-  static Future<Either<String, void>> verifySMSCode({required String phoneNumber, required String code}) {
+  Future<Either<String, void>> verifySMSCode({required String phoneNumber, required String code}) {
     return AntifraudFlutterPlatform.instance.verifySMSCode(code: code, phoneNumber: phoneNumber);
   }
 
-  static Future<Either<String, void>> detectFraud({required String code}) {
+  Future<Either<String, void>> detectFraud({required String code}) {
     return AntifraudFlutterPlatform.instance.detectFraud(code: code);
   }
 
-  static Future<Either<String, void>> makeOperation() {
+  Future<Either<String, void>> makeOperation() {
     return AntifraudFlutterPlatform.instance.makeOperation();
   }
 
-  static Future<Either<String, void>> confirmFace({required String document, required String birthDate}) {
+  Future<Either<String, void>> confirmFace({required String document, required String birthDate}) {
     return AntifraudFlutterPlatform.instance.confirmFace(document: document, birthDate: birthDate);
   }
 
-  static Future<Either<String, void>> logout() {
+  Future<Either<String, void>> logout() {
     return AntifraudFlutterPlatform.instance.logout();
   }
 }
